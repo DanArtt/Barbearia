481cd00d93f242bbdcd00472184fb82ddcf9fd8f
DanArtt <xdogman5@gmail.com>
2025-07-06 19:56:20 -0300

    dando inicio a criação da home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
-import { Button } from "./_components/ui/button"
+import Header from "@/components/header"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Text</Button>
+  return (
+    <div>
+      {/* Header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Daniel!</h2>
+        <p>Domingo, 06 de Julho</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Search..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Banner-01"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
